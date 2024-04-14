2f78cb7b75d6f72f7ae9cf325cc0738433e88555
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-04-14 05:00:08 +0300

    Add files via upload
    String Applications

---
+++ stringApplications.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 04:07:18 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is '+name+' '+surname+' and\n I am '+str(age)+' years old.'
+print(greeting)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+print('My name is  {} {} and I\'m {} years old.'.format(name, surname, age))
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+print('My name is  {2} {0} and I\'m {1} years old.'.format(name, surname, age))
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+print('My name is  {a} {n} and I\'m {s} years old.'.format(n=name, s=surname, a=age))
+'''
+
+'''
+cities=['Tokyo', 'Rio', 'London']
+plates=[1,2,3]
+print(plates[cities.index('Isparta')])
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+print(f'My name is  {name} {surname} and I\'m {age} years old.')
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is ' + name + ' ' + surname + ' and \n I am ' + str(age) + 'years old.'
+
+result1 = greeting[0]
+result2 = greeting[1]
+
+print(result1)
+print(result2)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is ' + name + ' ' + surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = greeting[-1]
+
+print(result)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is' + name +' '+ surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = len(greeting)
+
+print(result)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is' + name +' '+ surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = greeting[len(greeting)-1]
+
+print(result)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is ' + name +' '+ surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = greeting[8:17]
+
+print(result)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is ' + name +' '+ surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = greeting[8:]
+
+print(result)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is ' + name +' '+ surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = greeting[:17]
+
+print(result)
+'''
+
+'''
+name = '...'
+surname = '....'
+age = 20
+
+greeting = 'My name is ' + name +' '+ surname + ' and \n I am ' + str(age) + 'years old.'
+
+result = greeting[0:10:2]
+
+print(result)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
