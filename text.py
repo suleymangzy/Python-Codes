9ac9012eaf5821228a7aa1323d6df5a8a2312579
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-05-06 21:20:16 +0300

    Add files via upload
    Python program that prints the letters of the entered text one under the other

---
+++ text.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 23:45:41 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+def text():
+    inputText = input('Enter text: ')
+    for i in inputText:
+        if i == ' ':
+           i == ''
+        else:
+          print(i)
+           
+text()        
+    
