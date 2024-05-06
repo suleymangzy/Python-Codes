a0dd134a60203b275c9421093bcb7366a0ef279b
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-05-06 21:24:07 +0300

    Add files via upload
    Python calculates the area of ​​a rectangle whose width and height are entered using a function
    program

---
+++ rectangularArea.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  5 01:55:00 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+
+def rectangularArea():
+    height = int(input('Enter the height of the rectangle: '))
+    width = int(input('Enter the width of ​​the rectangle: '))
+    area = height*width
+    print('According to the values ​​you entered, the area of ​​your rectangle is: ',area)
+    
+rectangularArea()    
