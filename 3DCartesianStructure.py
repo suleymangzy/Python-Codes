96e23726c4fffaa91e16377ebc0c8c973fd1e610
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-06-02 01:46:57 +0300

    Add files via upload
    Creating a 3D Cartesian structure

---
+++ 3DCartesianStructure.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  1 19:19:41 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+fig=plt.figure()
+ax=fig.add_subplot(111,projection='3d')
+plt.show()
