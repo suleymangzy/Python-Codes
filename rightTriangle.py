9583b305b1f186e052e617d75b5d7cf86347d0a7
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-06-05 03:26:26 +0300

    Add files via upload
    Python code that creates a right triangle in line with the user's size input.

---
+++ rightTriangle.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 22:33:50 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+
+def rightTriangle():
+    size = int(input('Size: '))
+    for i in range(size+1):
+        print('*'*i,end='')
+        print()
+      
+rightTriangle()        
