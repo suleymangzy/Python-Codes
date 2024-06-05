39bac567752248182fe429f05e3fd26c776e01d1
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-06-05 03:21:21 +0300

    Add files via upload
    Python code that calculates numbers between 0-1000 that are divisible by 9 in a function called division and prints them on the screen.

---
+++ divide.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 22:24:50 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+
+def divide():
+    for i in range (0,1000):
+        if i % 9 == 0:
+            print(i,',',end='')
+
+divide() 
