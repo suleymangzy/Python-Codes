3503f052709a8be8d18773c9893d56674684df84
Süleyman Güzey <117369526+suleymangzy@users.noreply.github.com>
2024-06-05 03:25:16 +0300

    Add files via upload
    Python code that creates a square based on the user's size input.

---
+++ square.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  4 22:29:40 2024
+
+@author: SÜLEYMAN BEYYY
+"""
+
+def square():
+    size = int(input('Size: '))
+    for i in range(size):
+        print('*'*size,end='')
+        print()
+square()        
